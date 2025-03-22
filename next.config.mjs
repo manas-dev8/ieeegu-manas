e05b08075756a878356663dbac19f9c2dfad6c74
Anurag Kumar Singh <singh.ask@outlook.com>
2025-03-22 16:58:50 +0000

    Configure ESLint to ignore errors during builds

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com", "assets.aceternity.com","images.unsplash.com"]
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
