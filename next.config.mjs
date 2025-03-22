27430f1f54a9ce26971a12ed1d996cacc5056114
Anurag Kumar Singh <singh.ask@outlook.com>
2025-03-22 17:44:40 +0000

    Add Sanity CDN to image domains in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com", "assets.aceternity.com","images.unsplash.com"]
+    domains: ["cdn.sanity.io","res.cloudinary.com", "assets.aceternity.com","images.unsplash.com"]
   },
   eslint: {
     ignoreDuringBuilds: true,
